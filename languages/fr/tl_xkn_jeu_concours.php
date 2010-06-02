8d203318d88538f169d010995e51d88f16117255
olivier <olivier@astral.local>
2010-06-02 19:16:05 +0200

    ajout des boutons au fichier de langue

--- languages/fr/tl_xkn_jeu_concours.php
+++ languages/fr/tl_xkn_jeu_concours.php
@@ -1,7 +1,7 @@
 <?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
 
 // Fields
-$GLOBALS['TL_LANG']['tl_xkn_jeu_concours']['date_begin'] = array('Date de dÃ©but', '');
+$GLOBALS['TL_LANG']['tl_xkn_jeu_concours']['date_begin'] = array('Date de dŽbut', '');
 $GLOBALS['TL_LANG']['tl_xkn_jeu_concours']['date_end'] = array('Date de fin', '');
 $GLOBALS['TL_LANG']['tl_xkn_jeu_concours']['published'] = array('Publier','');
 $GLOBALS['TL_LANG']['tl_xkn_jeu_concours']['author'] = array('Auteur','');
@@ -16,4 +16,14 @@ $GLOBALS['TL_LANG']['tl_xkn_jeu_concours']['explication_texte'] = array('Texte a
 $GLOBALS['TL_LANG']['tl_xkn_jeu_concours']['legal'] = array('Condition legales','');
 $GLOBALS['TL_LANG']['tl_xkn_jeu_concours']['img_validation'] = array('Image de la page de validation du jeu','');
 $GLOBALS['TL_LANG']['tl_xkn_jeu_concours']['validation_texte'] = array('Texte associ&eacute; a l\'image de la page de validation','');
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_xkn_gallery']['new']    = array('Nouveau jeu', 'Cr&eacute;er un nouveau jeu');
+$GLOBALS['TL_LANG']['tl_xkn_gallery']['edit']   = array('Editer le jeu', 'Editer le jeu ID %s');
+$GLOBALS['TL_LANG']['tl_xkn_gallery']['editheader']   = array('Editer les r&eacute;glages du jeu', 'Editer  les r&eacute;glages du jeu ID %s');
+$GLOBALS['TL_LANG']['tl_xkn_gallery']['copy']   = array('Copier le jeu', 'Copier le jeu ID %s');
+$GLOBALS['TL_LANG']['tl_xkn_gallery']['delete'] = array('Supprimer le jeu', 'Supprimer le jeu ID %s');
+$GLOBALS['TL_LANG']['tl_xkn_gallery']['show']   = array('D&eacute;tails du jeu', 'Afficher les d&eacute;tails du jeu ID %s');
 ?>
